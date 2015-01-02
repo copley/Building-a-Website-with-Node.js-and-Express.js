4551c4c43405818dd0d7499c55b65856d232e203
marley-nodejs <jobpoisk@gmail.com>
2015-01-02 19:00:20 +0000

    03. Using Express.js | 03_03-Building a Hello Express application

---
+++ README.md
@@ -0,0 +1,6 @@
+Building-a-Website-with-Node.js-and-Express.js
+==============================================
+
+[Lynda.com] Building a Website with Node.js and Express.js [2014] 
+
+
--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.get('/', function(req, res) {
   res.send('<H1>Hello</H1> Express');
 });
 
-var server = app.listen(3000, function() {
-  console.log('Listening on port 3000');
-}); 
+var server = app.listen(8080, function() {
+  console.log('Listening on port 8080');
+});
